c8d0540fee1b46e471895329712a193760cbe20c
udonk <6565941+curryudon08@users.noreply.github.com>
2020-04-07 04:24:33 +0900

    演習問題の解答を追加

---
+++ 2_5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = long.Parse(Console.ReadLine());
+
+            var maxVal = long.MinValue;
+            var minVal = long.Parse(Console.ReadLine());
+            for(var i = 1; i < n; i++){
+                var r = long.Parse(Console.ReadLine());
+                maxVal = Math.Max(maxVal, r - minVal);
+                minVal = Math.Min(minVal, r);
+            }
+
+            Console.WriteLine(maxVal);
+        }
+    }
+}
