d3afcf9f404de7f9f71b251c1c3eba643d5a87dc
udonk <6565941+curryudon08@users.noreply.github.com>
2020-04-07 04:39:36 +0900

    演習問題の解答を追加

---
+++ 3_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var a = Console.ReadLine().Split().Select(i => int.Parse(i)).ToArray();
+
+            Console.WriteLine(string.Join(" ",a.Select(t => t.ToString())));
+            InsertionSort(a,n);
+        }
+
+        static void InsertionSort(int[] a, int n){
+            for(var i = 1; i < n; i++){
+                var v = a[i];
+                var j = i - 1;
+                while(j >= 0 && a[j] > v){
+                    a[j + 1] = a[j];
+                    j--;                
+                }
+                a[j + 1] = v;
+                Console.WriteLine(string.Join(" ",a.Select(t => t.ToString())));
+            }
+        }
+    }
+}
