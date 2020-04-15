2daa28939320472162aa690f8e81c083c3cb6cc5
udonk <6565941+curryudon08@users.noreply.github.com>
2020-04-16 01:27:47 +0900

    演習問題の解答を追加

---
+++ Chapter11/11_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _11_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            
+            var dp = new int[50];
+            dp[0] = 1;
+            dp[1] = 1;
+
+            for(var i = 2; i <=n; i++){
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+            Console.WriteLine(dp[n]);
+        }
+    }
+}
---
+++ Chapter11/11_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _11_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = int.Parse(Console.ReadLine());
+
+            for(var i = 0; i < x; i++){
+                var m = Console.ReadLine();
+                var n = Console.ReadLine();
+                Console.WriteLine(Lcs(1001, m, n));
+            }
+        }
+
+        static int Lcs(int size, string m, string n){
+            var c = new int[size,size];
+            var x =  " " + m;
+            var y =  " " + n;
+
+            var maxLen = 0;
+            for(var i = 1; i <= m.Length; i++){
+                for(var j = 1; j <= n.Length; j++){
+                    if(x[i] == y[j]){
+                        c[i, j] = c[i - 1, j - 1] + 1;
+                    }else{
+                        c[i, j] = Math.Max(c[i - 1, j], c[i, j - 1]);
+                    }
+                    maxLen = Math.Max(maxLen, c[i, j]);
+                }
+            }
+
+            return maxLen;
+        }
+    }
+}
